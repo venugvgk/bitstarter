6cfb50421c31d7a493a7066321457662c65bc045
venu GVGK <venu.gvgk@gmail.com>
2015-10-24 07:59:45 +0000

    one more try

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express()
-var fs = require('fs');
-var contents = fs.readFilesync('index.html').toString('utf 8');
-
+//var fs = require('fs');
+//var contents = fs.readFilesync('index.html').toString('utf 8');
+var contents = "variable display"
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('this is not working')
+  response.send(contents)
 })
 
 app.listen(app.get('port'), function() {
