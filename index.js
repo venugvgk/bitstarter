0cddbe7775dac3dce66187b4db08e74883e95419
venu GVGK <venu.gvgk@gmail.com>
2015-10-24 07:56:21 +0000

    one more try

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express()
-//var fs = require('fs');
-//var contents = fs.readFilesync('index.html').toString();
+var fs = require('fs');
+var contents = fs.readFilesync('index.html').toString('utf 8');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World - changed')
+  response.send(contents)
 })
 
 app.listen(app.get('port'), function() {
