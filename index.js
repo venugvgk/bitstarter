69e4582946e7c0e26550dd852e054e5281407a67
venu GVGK <venu.gvgk@gmail.com>
2015-10-24 07:50:39 +0000

    adding index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express()
-var fs = require('fs');
-var contents = fs.readFilesync('index.html').toString();
+//var fs = require('fs');
+//var contents = fs.readFilesync('index.html').toString();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('contents')
+  response.send('Hello World')
 })
 
 app.listen(app.get('port'), function() {
