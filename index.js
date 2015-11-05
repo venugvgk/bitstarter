4d8baa315626e21e69aaa2edb9e65ae1def06233
venu GVGK <venu.gvgk@gmail.com>
2015-11-05 13:54:23 +0000

    index.js changed for 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var contents = fs.readFileSync('index.html').toString('utf-8');
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
